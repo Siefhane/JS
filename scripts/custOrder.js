8fa0f95684bc1e705fc69db6196b5b87bf235ec8
siefhane <siefhane@gmail.com>
2023-08-14 05:24:16 +0300

    update

--- scripts/custOrder.js
+++ scripts/custOrder.js
@@ -5,13 +5,12 @@ var flag = false;
 function OrderNow(event) {
   document.getElementById("default").style.display = "block";
   document.getElementById("noCart").style.display = "none";
-  console.log(plate)
   var price = parseInt(
     document.getElementById(event.target.className).textContent
   );
-  arr.unshift(
-    document.getElementById("p" + event.target.className.substring(4)).innerText
-  );
+  // arr.unshift(
+  //   document.getElementById("p" + event.target.className.substring(4)).innerText
+  // );
   console.log(arr[0]);
   document.getElementById("subtotal").innerText = price;
   var fee = document.getElementById("fee").innerText;
