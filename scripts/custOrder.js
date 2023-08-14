614267772c290f3ab24e6330d54bb8cf19bd73d8
siefhane <siefhane@gmail.com>
2023-08-14 03:23:05 +0300

    new

--- scripts/custOrder.js
+++ scripts/custOrder.js
@@ -4,18 +4,28 @@ var flag = false;
 function OrderNow(event) {
   document.getElementById("default").style.display = "block";
   document.getElementById("noCart").style.display = "none";
-
+  console.log(plate)
   var price = parseInt(
     document.getElementById(event.target.className).innerText
   );
   console.log(price);
   document.getElementById("subtotal").innerText = price;
   var fee = document.getElementById("fee").innerText;
+  var fees = document.getElementById("fee");
   if (flag == false) {
     amount.innerText = parseInt(amount.innerText) + price + parseInt(fee);
   } else {
     amount.innerText = parseInt(amount.innerText) + price;
   }
-
+  let total= amount.innerText
+  let delevry=fees.innerText
+  setCookie("myCookie", total, delevry, 7);
   flag = true;
+}
+function setCookie(name, value1, value2, expirationDays) {
+  var combinedValue = value1 + ';' + value2;
+  var date = new Date();
+  date.setTime(date.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + encodeURIComponent(combinedValue) + ";" + expires + ";path=/";
 }
---
+++ scripts/js.js
@@ -0,0 +1,46 @@
+function getCookie(name) {
+    var cookieArr = document.cookie.split(";");
+  
+    for (var i = 0; i < cookieArr.length; i++) {
+      var cookiePair = cookieArr[i].split("=");
+  
+      if (name === cookiePair[0].trim()) {
+        return decodeURIComponent(cookiePair[1]);
+      }
+    }
+  
+    return null;
+  }
+  
+  // Retrieve the cookie value
+  var cookieValue = getCookie("myCookie");
+  
+  if (cookieValue) {
+    var values = cookieValue.split(';');
+    var value1 = values[0];
+    var value2 = values[1];
+    console.log(value1); 
+    console.log(value2); 
+  }
+var date= Date().substring(0,24);
+var randomNumber = Math.floor(Math.random() * 10001);
+var dateI=document.getElementById('dateNow')
+var CODE=document.getElementById('codeV')
+var Dvalue=document.getElementById('Dvalue')
+var input=document.getElementById('pay')
+var loading=document.getElementById('loading')
+var Tvalue=document.getElementById('Tvalue')
+dateI.innerText=date
+CODE.innerText=randomNumber
+Dvalue.innerText=value2
+Tvalue.innerText=value1
+var interval
+loading.style.display="none"
+function paynow(){
+    input.value=""
+        loading.style.display="flex"
+    interval=setInterval(()=>{
+        input.value="Pay now"
+        loading.style.display="none"
+        },8000)
+}
